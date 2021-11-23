b206deaff845ee13fb1083fc13da71b8a6f2e5a0
ajia <ajialeeholiday@gmail.com>
2021-11-23 14:20:34 -0700

    set up

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//Creat User model
+class User extends Model{}
+
+// define table columns and configuration
+User.init(
+    {
+        // TABLE COLUMN DEFINITIONS
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            timestamps: false,
+            freezeTableName: true,
+            underscored: true,
+            modelName: 'user'
+        }
+    },
+    {
+        // TABLE CONFIGURATION
+
+        // pass in imported sequelize connection/direct connection
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+
+    }
+);
+
+module.exports = User;
